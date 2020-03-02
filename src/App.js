67e4bcb2f73e4fd188e5a5141acc51b3d517aa86
David <davidmaillo90@gmail.com>
2020-03-02 13:59:25 +0800

    Imported React-router-dom + Added routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Chat from './Chat'
+import Login from './Login'
+import Signup from './Signup'
 import './styles/App.css'
 
 class App extends Component {
 	// Methods
 	// Render
 	render() {
-		return <Chat />
+		return (
+			<BrowserRouter>
+				<Switch>
+					<Route path="/login" component={Login}></Route>
+					<Route path="/signup" component={Signup}></Route>
+					<Route path="/" component={Chat}></Route>
+				</Switch>
+			</BrowserRouter>
+		)
 	}
 }
 
